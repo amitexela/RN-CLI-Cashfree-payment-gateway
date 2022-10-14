8be4ee10fd1d019599f9a97ac45b5a84d1b7af00
Amit Rathee <amit@ubuntu.com>
2022-10-14 19:15:17 +0530

    added comments

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import { CFPaymentGatewayService, } from 'react-native-cashfree-pg-sdk';
 import { CFDropCheckoutPayment, CFEnvironment, CFPaymentComponentBuilder, CFPaymentModes, CFSession, CFThemeBuilder, } from 'cashfree-pg-api-contract';
 import axios from 'axios';
 
-const BASE_RESPONSE_TEXT = 'Response or error will show here.';
+const BASE_RESPONSE_TEXT = 'Response or error will show here after payment.';
 
 export default class App extends Component {
     constructor() {
         super();
         this.state = {
             responseText: BASE_RESPONSE_TEXT,
+            amount: 10
         };
     }
     onVerify(orderID) {
@@ -38,15 +39,15 @@ export default class App extends Component {
           // -------------- this should be in backend
           var data = JSON.stringify({
             "customer_details": {
-              "customer_id": "7112AAA812234",
-              "customer_email": "johny@cashfree.com",
-              "customer_phone": "9908734801"
+              "customer_id": "7112AAA812234",             // payerId (PID)
+              "customer_email": "johny@cashfree.com",     // payerEmail
+              "customer_phone": "9908734801"              // payerPhone
             },
             "order_meta": {
               "notify_url": "https://webhook.site/0578a7fd-a0c0-4d47-956c-d02a061e36d3"
             },
-            "order_amount": 1,
-            "order_currency": "INR"
+            "order_amount": this.state.amount,                 // payment amount
+            "order_currency": "INR"            // payment currency
           });
 
           var config = {
@@ -56,17 +57,21 @@ export default class App extends Component {
               'Accept': 'application/json', 
               'x-api-version': '2022-01-01', 
               'Content-Type': 'application/json', 
-              'x-client-id': 'TEST2508600dd5e86f258119f5cc49068052', 
-              'x-client-secret': 'TEST19a32a360b26b348f2bb1336acdadc42b25c00a'
+              'x-client-id': 'TEST2508600dd5e86f258119f5cc49068052',               // biller cashfree appId
+              'x-client-secret': 'TEST19a32a360b26b348f2bb1336acdadc42b25c00a'     // biller cashfree secret key
             },
             data : data
           };
+          // git remote add origin command
+          // git remote add origin https://github.com/amitexela/RN-Razorpay-payment-gateway.git
+          // git remote add origin https://github.com/amitexela/RN_CLI_UPI.git
+          // 
           // -------------- end
           const response = await axios(config);
           console.log('response', response.data.order_token, response.data.order_id);
             const session = new CFSession(
-              response.data.order_token,
-              response.data.order_id,
+              response.data.order_token,     // comes from above api call (from cashfree backend)
+              response.data.order_id,        // comes from above api call (from cashfree backend)
               CFEnvironment.SANDBOX
               );
             const paymentModes = new CFPaymentComponentBuilder()
@@ -94,10 +99,13 @@ export default class App extends Component {
     render() {
         return (
           <View style={styles.container}>
-            <Text style={styles.welcome}>Welcome to React Native!</Text>
+            <Text style={styles.welcome}>Welcome to Cashfree Payment POC</Text>
+            <Text style={styles.welcome}>in React Native CLI!</Text>
             <View style={styles.buttonContainer}>
-              <Button title="Start Checkout" onPress={() => this._startCheckout()} />
+              <Button title={`Pay Now with Cashfree`} onPress={() => this._startCheckout()} />
             </View>
+            <Text style={styles.instructions}>Payment amount = {this.state.amount} INR</Text>
+
             <Text style={styles.instructions}>{this.state.responseText}</Text>
           </View>
         );
